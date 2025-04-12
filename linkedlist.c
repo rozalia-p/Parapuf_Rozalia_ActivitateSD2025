1c3570b803680cc93070aae8d2bf38da5a6bff09
Rozalia <rozalia1337@gmail.com>
2025-04-12 21:31:31 +0300

    update linkedlist

--- linkedlist.c
+++ linkedlist.c
@@ -1,38 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 struct Node {
-	int data;
-	struct Node* next;
+    int data;
+    struct Node* next;
 };
-struct Node* head;//variabila globala
-void Insert(int x) {
-	struct Node* temp = (Node*)malloc(sizeof(struct Node));
-	temp->data = x;
-	temp->next = head;
-	head = temp;
 
+struct Node* head; // variabila globala
+
+void Insert(int x) {
+    struct Node* temp = malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = head;
+    head = temp;
 }
+
 void Print() {
-	struct Node* temp = head;
-	printf("List is: ");
-	while (temp != NULL)
-	{
-		printf(" %d", temp->data);
-		temp = temp->next;
-	}
-	printf("\n");
-};
+    struct Node* temp = head;
+    printf("Lista este:");
+    while (temp != NULL) {
+        printf(" %d", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
 
 int main() {
-	head = NULL; // lista goala
-	printf("Cate numere?\n");
-	int n, i, x;
-	scanf("%d", &n);
-	for (i = 0; i < n; i++) {
-		printf("Introduceti numarul \n");
-		scanf("%d", &x);
-		Insert(x);
-		Print();
-	}
+    head = NULL; // lista este goala la început
+    int n, x;
+
+    printf("Cate numere?\n");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Introduceti numarul:\n");
+        scanf("%d", &x);
+        Insert(x);
+        Print();
+    }
+
+    return 0;
 }
